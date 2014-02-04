4ab0f247fb2bd2b285316911d08536ab268250cb
Alexey Alekhin <laughedelic@gmail.com>
2014-02-04 20:25:45 +0100

    Removed --self-contained flag

--- compile.revealjs.sh
+++ compile.revealjs.sh
@@ -4,6 +4,4 @@ pandoc -t revealjs --template=template.revealjs \
     --slide-level=2 \
     --variable theme="moon" \
     --variable transition="linear" \
-    --self-contained \
-    $1.revealjs.md -o $1.revealjs.full.html
-
+    $1.revealjs.md -o $1.revealjs.html
